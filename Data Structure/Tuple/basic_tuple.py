3afba901548b5c2091dabde0492172e36aa7d661
Ansh mishra <anshmishra9415@gmail.com>
2020-12-25 01:56:49 +0530

    repository created

---
+++ Data Structure/Tuple/basic_tuple.py
@@ -0,0 +1,15 @@
+#tuple = tu = ()
+tup = (2,8)
+tup1 = (111,2,13,4,4)
+tup2 = ('a','n','s','h')
+tup3 = ("abc","efg","hij")
+tup4 = ({"ansh","pcm"},{"pratik","bio"})
+print(tup1)
+tupx = tup+tup1
+print(tupx)
+
+print(len(tup3))
+print(tup1.index(1))
+print(sorted(tup1))
+
+
---
+++ Data Structure/Tuple/postive_tup.py
@@ -0,0 +1,6 @@
+tup = (-1,4,3,-45,-23,2)
+tupx= ()
+for i in tup:
+    if i>0:
+        tupx+=(i,) #we have to put comma else it will become an string and then it will give an error.
+print(tupx)
